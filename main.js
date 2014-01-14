4eff1c1b68c800c5f92854b4f0ff0fdcee5c7c8e
Andrew Shaffer <andrew@weo.io>
2014-01-13 16:06:53 -0800

    added ObjectStack utility

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 angular.module('angils', [])
 .factory('promiseStatus', ['$q', function($q) {
   return function(promise) {
@@ -13,4 +12,66 @@ angular.module('angils', [])
     });
     return promise;
   }
-}]);
+}])
+.factory('ObjectStack', function() {
+  function ObjectStack(defaults) {
+    this.__history = [];
+    this.__defaults = defaults || {};
+    _.extend(this, defaults);
+  }
+
+  function isObjectStackKey(key) {
+    return key.indexOf('__') === 0;
+  }
+
+  ObjectStack.prototype.push = function() {
+    this.__history.push(_.reduce(this, function(memo, val, key) {
+      if(! isObjectStackKey(key))
+        memo[key] = val;
+      return memo;
+    }, {}));
+    return this;
+  };
+
+  ObjectStack.prototype.set = function(fields) {
+    _.extend(this, fields);
+    return this;
+  };
+
+  ObjectStack.prototype.pop = function() {
+    this.clear();
+    _.extend(this, this.__history.pop());
+    return this;
+  };
+
+  ObjectStack.prototype.clear = function() {
+    _.each(this, function(val, key) {
+      if(! isObjectStackKey(key))
+        delete this[key];
+    }, this);
+    _.extend(this, this.__defaults);
+    return this;
+  };
+
+  ObjectStack.prototype.history = function() {
+    var copy = this.__history.slice(0);
+    copy.unshift(this.__defaults);
+    return copy;
+  };
+
+  ObjectStack.prototype.last = function() {
+    return _.last(this.__history);
+  };
+
+  ObjectStack.prototype.reset = function() {
+    this.clear();
+    this.__history = [];
+    return this;
+  };
+
+  ObjectStack.prototype.size = function() {
+    return this.__history.length;
+  };
+
+  return ObjectStack;
+});
